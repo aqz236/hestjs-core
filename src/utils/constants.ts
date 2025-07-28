857ba5a1aa858e8dd7c560526b2516aeb6d499a4
aqz236 <2367814400@qq.com>
2025-07-28 18:17:02 +0800

    refactor(utils): 更新元数据键为全局唯一符号
    - 将元数据键的 Symbol 改为全局唯一的 Symbol.for
    - 添加 'hest:' 前缀以避免潜在的键冲突
    - 此修改有助于确保元数据键在全局范围内的唯一性，提高代码的健壮性

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,12 @@
 // 元数据键常量
 export const METADATA_KEYS = {
-  CONTROLLER: Symbol('controller'),
-  INJECTABLE: Symbol('injectable'),
-  MODULE: Symbol('module'),
-  ROUTE: Symbol('route'),
-  PARAM: Symbol('param'),
-  MIDDLEWARE: Symbol('middleware'),
-  GUARD: Symbol('guard'),
+  CONTROLLER: Symbol.for('hest:controller'),
+  INJECTABLE: Symbol.for('hest:injectable'),
+  MODULE: Symbol.for('hest:module'),
+  ROUTE: Symbol.for('hest:route'),
+  PARAM: Symbol.for('hest:param'),
+  MIDDLEWARE: Symbol.for('hest:middleware'),
+  GUARD: Symbol.for('hest:guard'),
 } as const;
 
 // HTTP 方法枚举
